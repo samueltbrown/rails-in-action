5f6813bb15a5c9831e90aa22001744e98781d57e
Sam Brown <samuel.t.brown@gmail.com>
2013-06-24 23:03:43 +0000

    Updated initial rails app with cucumber and rspec

---
+++ ticketee/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'b8383edcd389b71a45c595e6ce1b6bd88fdf1945953d7588cefe4704d1c230fc14fe4a34446e022c8d429771cbe2606f11e13e00969142adcf2620242f63836d'
