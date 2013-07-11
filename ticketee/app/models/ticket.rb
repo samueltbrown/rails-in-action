07181965675eeef1b902d37685a121aee6500034
Samuel Brown <samuel.t.brown@gmail.com>
2013-07-10 21:57:29 -0400

    Added ticket validation scenarios

--- ticketee/app/models/ticket.rb
+++ ticketee/app/models/ticket.rb
@@ -1,5 +1,6 @@
 class Ticket < ActiveRecord::Base
   attr_accessible :title, :description, :project
   validates :title, :presence => true
-  validates :description, :presence => true
+  validates :description, :presence => true ,
+                          :length => {:minimum => 10}
 end
